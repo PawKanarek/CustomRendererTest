46075072294b0ccc200dfa468b5088098bd529a6
PawelK <p.kanarek@insys.pl>
2019-05-31 11:12:51 +0200

    initial commit

---
+++ CustomRendererTest/CustomRendererTest/CustomRendererTest.iOS/CustomRenderer.cs
@@ -0,0 +1,36 @@
+﻿using CustomRendererTest;
+using CustomRendererTest.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomView), typeof(CustomRenderer))]
+namespace CustomRendererTest.iOS
+{
+    public class CustomRenderer : ViewRenderer<CustomView, UIView>
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<CustomView> e)
+        {
+            base.OnElementChanged(e);
+
+            //Pattern taken from: https://docs.microsoft.com/en-us/xamarin/xamarin-forms/app-fundamentals/custom-renderer/view
+            if (this.Control == null)
+            {
+                // Instantiate the native control and assign it to the Control property with
+                // the SetNativeControl method
+                UIView myView = new UIView();
+                this.SetNativeControl(myView);
+            }
+
+            if (e.OldElement != null)
+            {
+                // Unsubscribe from event handlers and cleanup any resources
+            }
+
+            if (e.NewElement != null)
+            {
+                // Configure the control and subscribe to event handlers
+            }
+        }
+    }
+}
---
+++ CustomRendererTest/CustomRendererTest/CustomRendererTest/App.cs
@@ -0,0 +1,30 @@
+﻿using Xamarin.Forms;
+
+namespace CustomRendererTest
+{
+    public class CustomView : View
+    {
+    }
+
+    public class App : Application
+    {
+        public App()
+        {
+            var rootPage = new ContentPage();
+            var navigationPage = new NavigationPage(rootPage);
+            var button = new Button()
+            {
+                Text = "Click me",
+                Command = new Command(() => navigationPage.PushAsync(new ContentPage()
+                {
+                    Content = new CustomView()
+                }))
+            };
+            var content = new StackLayout();
+            content.Children.Add(button);
+            content.Children.Add(new CustomView());
+            rootPage.Content = content;
+            this.MainPage = navigationPage;
+        }
+    }
+}
